4773996ef029856e2743eb39d38607e8a0c94cfe
THEYAKUZI <defianteagle@gmail.com>
2026-01-04 19:36:53 +0000

    Remove Community Forge Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ShopPage from './pages/ShopPage';
-import CommunityForgePage from './pages/CommunityForgePage';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ShopPage />} />
-        <Route path="/community-forge" element={<CommunityForgePage />} />
       </Routes>
     </Router>
   );
